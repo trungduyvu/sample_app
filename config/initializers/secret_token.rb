6c41d9641c18624d47b10cc17d0426134d3b9994
tvu <tvu@homeaway.com>
2012-04-23 16:25:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '76a74267ee0834a95f9d749a90dcc339b9d280a3267fb87ba93489fc6bdfd3f3ef2687d17959cd7e8238d096da6ca3158d4513cb1dfd53b1f2d0b69043e1a775'
